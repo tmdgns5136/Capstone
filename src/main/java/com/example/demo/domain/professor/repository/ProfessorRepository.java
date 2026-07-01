200276c286578ef1bde329250d7c667b5d07dd4f
이승훈 <tmdgns5136@naver.com>
2026-07-01 21:53:46 +0900

    merge

--- src/main/java/com/example/demo/domain/professor/repository/ProfessorRepository.java
+++ src/main/java/com/example/demo/domain/professor/repository/ProfessorRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-    Professor findByLecture(Lecture lecture);
 
     Professor findByProfessorNum(String professorNum);
 
